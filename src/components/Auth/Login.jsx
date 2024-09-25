75bda38472371083fe024dfdc8b4ddb360977ef2
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-26 00:27:40 +0530

    add functionality

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { login } from "../../Redux/Slices/AuthSlice";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleChange = ({ target: { name, value } }) => {
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
-    // Add your login logic here
+    const { username, password } = formData;
+
+    if (!username || !password) {
+      return toast.error("Please fill all required fields");
+    }
+
+    const { payload } = await dispatch(login(formData));
+
+    console.log({payload: payload});
+    
+
+    if (payload === 'Invalid Password' || payload === 'Invalid credentials' ) {
+      return toast.error(payload.error || "Login failed");
+    }
+
+    toast.success("Login successful");
+    navigate("/");
   };
 
   return (
@@ -21,36 +39,23 @@ const Login = () => {
       <div className="w-full max-w-md bg-white p-8 shadow-lg rounded-lg">
         <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-              Username
-            </label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              value={formData.username}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your username"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your password"
-              required
-            />
-          </div>
+          {["username", "password"].map((field) => (
+            <div key={field} className="mb-4">
+              <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={field}>
+                {field.charAt(0).toUpperCase() + field.slice(1)}
+              </label>
+              <input
+                type={field}
+                name={field}
+                id={field}
+                value={formData[field]}
+                onChange={handleChange}
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                placeholder={`Enter your ${field}`}
+                required
+              />
+            </div>
+          ))}
           <div className="flex items-center justify-between">
             <button
               type="submit"
