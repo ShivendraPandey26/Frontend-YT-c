bcab55261b040a3e3f4bbddc9c9c56d975972e8e
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-25 20:41:56 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 // @Component
 import Home from "./components/home/home";
 import Signup from "./components/Auth/Signup";
-import Login from "./components/Auth/Login"
+import Login from "./components/Auth/Login";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   );
 }
 
