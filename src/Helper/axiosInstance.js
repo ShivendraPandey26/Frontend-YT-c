022730375f3733df3913d8c0fd20fd0952313423
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-25 20:42:37 +0530

    create axios helper

---
+++ src/Helper/axiosInstance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BackendURL = 'https://youtube-clone-sp.onrender.com/api/v1/'
+
+const axiosInstance = axios.create();
+
+axiosInstance.defaults.baseURL = BackendURL;
+axiosInstance.defaults.withCredentials = true;
+
+export default axiosInstance;
