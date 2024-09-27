55bcc2aedad2705389e9812be13a07934513845e
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-27 16:45:39 +0530

    add more Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 // @Component
-import Home from "./components/home/home";
+import Home from "./components/Home/Home";
 import Signup from "./components/Auth/Signup";
 import Login from "./components/Auth/Login";
+import Watch from "./components/video/Watch";
+import PublishVideo from "./components/Video/PublishVideo";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Route exact path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/watch/:videoId" element={<Watch />} />
+      <Route path="/publish-video" element={<PublishVideo />} />
     </Routes>
   );
 }
