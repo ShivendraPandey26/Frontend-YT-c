c12d6dcf909a6ac427f506328e359821b2fc1815
Shivendra Kumar Pandey <aviralpandey7974@gmail.com>
2024-09-30 02:18:23 +0530

    create

---
+++ src/components/Subscribe/Subscribe.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Layout from "../Layout/Layout";
+
+function Subscribe() {
+  return (
+    <Layout>
+      <div className="w-full h-[70vh]">
+        <h1 className="text-3xl my-[40vh] text-center">
+          This component is coming soon and will be available shortly. Thank you
+          for your patience!
+        </h1>
+      </div>
+    </Layout>
+  );
+}
+
+export default Subscribe;
